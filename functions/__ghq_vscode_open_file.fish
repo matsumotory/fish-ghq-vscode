52037b66103cf5da3040e3e3f10769aef438edf1
matsumotory <matsu1229@gmail.com>
2020-10-09 14:22:43 +0900

    Support other selectors like fzf fzf-tmux peco percol fzy sk

--- functions/__ghq_vscode_open_file.fish
+++ functions/__ghq_vscode_open_file.fish
@@ -1,8 +1,27 @@
 function __ghq_vscode_open_file -d "Open a file in a git repository by vscode" 
-  set repo (ghq list --full-path | fzf)
+  # ref: __ghq_repository_search.fish Thanks!
+  set -l selector
+  [ -n "$GHQ_SELECTOR" ]; and set selector $GHQ_SELECTOR; or set selector fzf
+
+  set -l selector_options
+  [ -n "$GHQ_SELECTOR_OPTS" ]; and set selector_options $GHQ_SELECTOR_OPTS
+
+  if not type -qf $selector
+    printf "\nERROR: '$selector' not found.\n"
+    return 1
+  end
+
+  switch "$selector"
+    case fzf fzf-tmux peco percol fzy sk
+      set repo (ghq list --full-path | "$selector" $selector_options)
+    case \*
+      printf "\nERROR: fish-ghq and fish-ghq-vscode is not support '$selector'.\n"
+  end
+
   if test -n "$repo" 
     cd 
-    set file (cd $repo; and fzf)
+    # ref: kentaro/fish-ghq-vscode Thanks!
+    set file (cd $repo; and git ls-files | "$selector" $selector_options)
     if test -n "$file"
       code "$repo"/"$file"
     end
